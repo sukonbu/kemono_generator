6bf55e7ae868a12c7aa2a9b5626d54d07d65a044
sukonbu <t.cfhhy.skn0909@gmail.com>
2017-02-14 16:59:30 +0900

    added mokemoke mode

--- kemono.py
+++ kemono.py
@@ -4,18 +4,14 @@ import random
 list = ["け", "も", "の"]
 nokemono = ["の", "け", "も", "の"]
 mononoke = ["も", "の", "の", "け"]
-
+mokemoke = ["も", "け", "も", "け"]
 nokemono_list = ["", "", "", ""]
-kemono_list = ["", "", ""]
-nokemono_count = 0
 count = 0
 
 while True:
     count += 1
     rand_str = list[random.randint(0, len(list) - 1)]
     print(rand_str, end='')
-    kemono_list.pop(0)
-    kemono_list.append(rand_str)
     nokemono_list.pop(0)
     nokemono_list.append(rand_str)
     # print(str(kemono_list) + str(nokemono_list))
@@ -27,7 +23,11 @@ while True:
         print(" ")
         print("あなたは" + str(count) + "回でシシ神になりました ")
         break
-    if kemono_list == list:
+    if nokemono_list == mokemoke:
+        print(" ")
+        print("あなたは" + str(count) + "回でもけもけを通常召喚しました ")
+        break
+    if nokemono_list[1:] == list:
         print(" ")
         print("あなたは" + str(count) + "回でけものになりました ")
         break
